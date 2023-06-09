108f2b8f99f9ec5f9d69ff81f453d223d7c80812
- <wavevape@gmail.com>
2023-06-09 22:29:08 +0300

    чуть чистка

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import GoodsAfterSearch from "./components/goods/goodsAfterSearch/goodsAfterSearch/GoodsAfterSearch";
 import Header from "./components/header/Header";
-import LeftNavigationAfterSearch from "./components/leftnavigation/afterSearch/LeftNavigationAfterSearch";
 import style from "./styles/App.module.css";
 import "../src/index";
-import Cart from "./components/cart/Cart";
 import RouteNavigation from "./components/Routes/RouteNavigation";
-
-import Error from "./components/Errors/Error";
-import { NavLink, Route, Routes } from "react-router-dom";
-import PageAfterSearch from "./components/Pages/PageAfterSearch";
 
 function App() {
 	return (
 		<div>
 			<Header />
-
 			<div className={style.container}>
-				<Routes>
-					<Route path="/cart" element={<Cart />} />
-					<Route path="/search" element={<PageAfterSearch />} />
-					<Route path="*" element={<Error />} />
-				</Routes>
+				<RouteNavigation />
 			</div>
 		</div>
 	);
--- src/components/Routes/RouteNavigation.jsx
+++ src/components/Routes/RouteNavigation.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Cart from "../cart/Cart";
+import PageAfterSearch from "../Pages/PageAfterSearch";
+import Error from "../Errors/Error";
 
 const RouteNavigation = () => {
 	return (
 		<Routes>
 			<Route path="/cart" element={<Cart />} />
+			<Route path="/" element={<PageAfterSearch />} />
+			<Route path="*" element={<Error />} />
 		</Routes>
 	);
 };
